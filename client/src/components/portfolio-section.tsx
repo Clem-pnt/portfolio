53615bf052e681385dbe10b545a8322d48609fe9
Clem-pnt <138514716+Clem-pnt@users.noreply.github.com>
2025-10-10 22:12:17 +0200

    Update portfolio-section.tsx

--- client/src/components/portfolio-section.tsx
+++ client/src/components/portfolio-section.tsx
@@ -1,7 +1,7 @@
 import { ExternalLink } from "lucide-react";
 import { SiDiscord } from "react-icons/si";
 import aragoniteBanner from "@assets/aragonite-banner_1757291789566.png";
-import goliathBanner from "@assets/d3462f57abcfee7628b6d67a6415ef12_1757291813344.png";
+import goliathBanner from "@assets/goliath_banner.png";
 import gladosBanner from "@assets/69dd83a9e0d1202ee87af3a06b1bb520_1757291832036.png";
 import croissantBanner from "@assets/croissant.png";
 
